d64abcae4d6674c1b84d9c26405511c92b5d03bd
danielholmes839 <daniel.holmes@dapperlabs.com>
2022-07-23 15:14:18 -0400

    update schedule index

--- db/schedule_index.go
+++ db/schedule_index.go
@@ -56,46 +56,49 @@ type ScheduleResults struct {
 	results []model.StopTime
 }
 
-func (s *ScheduleResults) Next(after time.Time, limit int) []model.StopTime {
-	results := s.nextWithinDay(after, limit)
+func (s *ScheduleResults) Before(before time.Time, limit int) []model.StopTime {
+	results := s.beforeWithinDay(before, limit)
 
+	before = truncate(before).Add(-time.Second)
 	attempts := 0
-	for len(results) != limit && attempts < 14 {
+	for len(results) != limit && attempts < 3 {
+		fmt.Println(before)
 		attempts++
-		after = truncate(after.AddDate(0, 0, 1))
-		results = append(results, s.nextWithinDay(after, limit-len(results))...)
+		results = append(results, s.beforeWithinDay(before, limit-len(results))...)
+		before = before.AddDate(0, 0, -1)
 	}
 
 	return results
 }
 
-func (s *ScheduleResults) Day(on time.Time) []model.StopTime {
-	return s.nextWithinDay(truncate(on), -1)
+// query next N stop times after a specific time
+func (s *ScheduleResults) After(after time.Time, limit int) []model.StopTime {
+	results := s.afterWithinDay(after, limit)
+
+	attempts := 0
+	for len(results) != limit && attempts < 3 {
+		attempts++
+		after = truncate(after.AddDate(0, 0, 1))
+		results = append(results, s.afterWithinDay(after, limit-len(results))...)
+	}
+
+	return results
 }
 
-func (s *ScheduleResults) nextWithinDay(after time.Time, limit int) []model.StopTime {
+// query all stop times on a specific date
+func (s *ScheduleResults) Day(on time.Time) []model.StopTime {
+	return s.afterWithinDay(truncate(on), -1)
+}
+
+func (s *ScheduleResults) afterWithinDay(t time.Time, limit int) []model.StopTime {
 	results := []model.StopTime{}
 
 	for _, stopTime := range s.results {
-		if !ahead(stopTime.Time, after) {
+		if !after(stopTime.Time, t) {
 			continue
 		}
 
-		trip, _ := s.trips.Get(stopTime.TripId)
-		service, _ := s.services.Get(trip.ServiceId)
-
-		// results must be between the start and end dates
-		if service.End.Before(after) || service.Start.After(after) {
-			continue
-		}
-
-		// results must have service on the given day
-		if !service.On[after.Weekday()] {
-			continue
-		}
-
-		// results must not have execpetions
-		if exception, exists := s.serviceExceptions.Get(service.Id, after); exists && !exception.Added {
+		if !s.valid(t, stopTime) {
 			continue
 		}
 
@@ -109,19 +112,87 @@ func (s *ScheduleResults) nextWithinDay(after time.Time, limit int) []model.Stop
 	return results
 }
 
-func ahead(a, b time.Time) bool {
-	// if a is ahead of b
-	if a.Hour() >= b.Hour() {
+func (s *ScheduleResults) beforeWithinDay(t time.Time, limit int) []model.StopTime {
+	results := []model.StopTime{}
+
+	for _, stopTime := range reverse(s.results) {
+		if !before(stopTime.Time, t) {
+			continue
+		}
+
+		if !s.valid(t, stopTime) {
+			continue
+		}
+
+		results = append(results, stopTime)
+
+		if len(results) == limit {
+			break
+		}
+	}
+
+	return results
+}
+
+/* valid stopTime given a specific time
+- the service start and end date include the time
+- the service is running on the time's day of the week
+- there on no service exception on the time's date
+*/
+
+func (s *ScheduleResults) valid(t time.Time, stopTime model.StopTime) bool {
+	trip, _ := s.trips.Get(stopTime.TripId)
+	service, _ := s.services.Get(trip.ServiceId)
+
+	// results must be between the start and end dates
+	if service.End.Before(t) || service.Start.After(t) {
+		return false
+	}
+
+	// results must have service on the given day
+	if !service.On[t.Weekday()] {
+		return false
+	}
+
+	// results must not have execpetions
+	if exception, exists := s.serviceExceptions.Get(service.Id, t); exists && !exception.Added {
+		return false
+	}
+
+	return true
+}
+
+// if a is ahead of b (hours and minutes)
+func after(a, b time.Time) bool {
+	if a.Hour() > b.Hour() {
 		return true
-	} else if a.Hour() == b.Hour() && a.Minute() > b.Minute() {
+	} else if a.Hour() == b.Hour() && a.Minute() >= b.Minute() { // minutes can be equal
 		return true
 	}
 	return false
 }
 
+// if a is before of b (hours and minutes)
+func before(a, b time.Time) bool {
+	if a.Hour() < b.Hour() {
+		return true
+	} else if a.Hour() == b.Hour() && a.Minute() >= b.Minute() { // minutes can be equal
+		return true
+	}
+	return false
+}
+
+// truncate time leaving only the date
 func truncate(t time.Time) time.Time {
-	// truncate time leaving only the date
 	_, offset := t.Zone()
 	sub := time.Duration(offset) * time.Second
 	return t.Add(-sub).Truncate(time.Hour * 24).Add(-sub)
+}
+
+func reverse(data []model.StopTime) []model.StopTime {
+	reversed := make([]model.StopTime, len(data))
+	for i, stopTime := range data {
+		reversed[len(data)-(i+1)] = stopTime
+	}
+	return reversed
 }
