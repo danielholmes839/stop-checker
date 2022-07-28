77703f2b51d2f6609a47357f7e10d1330da6201f
danielholmes839 <daniel.holmes@dapperlabs.com>
2022-07-27 23:53:19 -0400

    separate GTFS  models

--- db/model/gtfs.go
+++ db/model/gtfs.go
@@ -2,7 +2,6 @@ package model
 
 import (
 	"fmt"
-	"math"
 	"time"
 )
 
@@ -65,17 +64,6 @@ type Stop struct {
 	Type string
 }
 
-type StopRoute struct {
-	RouteId     string
-	StopId      string
-	DirectionId string
-	Headsign    string
-}
-
-func (r StopRoute) DirectedID() string {
-	return fmt.Sprintf("%s:%s", r.RouteId, r.DirectionId)
-}
-
 func (s Stop) ID() string {
 	return s.Id
 }
@@ -91,32 +79,4 @@ type Trip struct {
 
 func (t Trip) ID() string {
 	return t.Id
-}
-
-type Location struct {
-	Latitude  float64
-	Longitude float64
-}
-
-const earthRaidusKm float64 = 6371 // radius of the earth in kilometers.
-
-func degreesToRadians(d float64) float64 {
-	return d * math.Pi / 180
-}
-
-func (l Location) Distance(other Location) float64 {
-	lat1 := degreesToRadians(l.Latitude)
-	lon1 := degreesToRadians(l.Longitude)
-	lat2 := degreesToRadians(other.Latitude)
-	lon2 := degreesToRadians(other.Longitude)
-
-	diffLat := lat2 - lat1
-	diffLon := lon2 - lon1
-
-	a := math.Pow(math.Sin(diffLat/2), 2) + math.Cos(lat1)*math.Cos(lat2)*
-		math.Pow(math.Sin(diffLon/2), 2)
-
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-
-	return c * 6_371_000
 }
