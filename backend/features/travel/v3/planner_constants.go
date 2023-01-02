bea612909ea1c2b2b309bd47fdbf2ba82274acda
Daniel <danielh839@gmail.com>
2023-01-02 15:39:19 -0500

    renamed

---
+++ backend/features/travel/v3/planner_constants.go
@@ -0,0 +1,61 @@
+package v3
+
+import (
+	"math"
+	"time"
+
+	"stop-checker.com/db/model"
+)
+
+const MAX_WALK = 300.0
+const MAX_WALK_INITIAL = 1000.0
+const MAX_WALK_TARGET = 1000.0
+
+const TRANSFER_PENALTY = 5 * time.Minute
+
+// value multiplied by distance remaining to the stop (3 minute penalty per km away)
+// typical A* heuristic
+const DISTANCE_PENALTY = (3 * time.Minute) / 1000
+
+// walking constants
+const WALK_SPEED = 1.4 // meters per second
+const WALK_PENALTY = 1
+
+type Kind int
+
+const (
+	INITIAL Kind = iota
+	TARGET
+	STOP
+)
+
+type Mode int
+
+const (
+	DEPART_AT Mode = iota
+	ARRIVE_BY
+)
+
+func walkingDuration(distance float64) time.Duration {
+	duration := time.Duration(math.Round(distance/(WALK_SPEED*60))) * time.Minute
+	if duration < time.Minute {
+		return time.Minute
+	}
+	return duration
+}
+
+func walkingDurationContinuous(distance float64) time.Duration {
+	duration := time.Duration(distance) * time.Minute / (WALK_SPEED * 60)
+	if duration < time.Minute {
+		return time.Minute
+	}
+	return duration
+}
+
+type walkingDirections interface {
+	GetDirections(origin, destination model.Location) (model.Path, error)
+}
+
+type walkingDirectionsCache interface {
+	GetDirections(originId, destinationId string) (model.Path, error)
+}
